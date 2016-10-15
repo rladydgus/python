f41b90c4cb56d7ea0fd990f7a8ec743bfb35735a
rladydgus <dydgus1581@gmail.com>
2016-10-15 12:17:53 +0900

    Add files via upload

---
+++ count.py
@@ -0,0 +1,4 @@
+count=0
+for itervar in[3,42,12,9,74,15]:
+        count=count+1
+print('Count:',count)
---
+++ for2.py
@@ -0,0 +1,4 @@
+friends =['조진웅','송중기','송혜교','김혜수']
+for friend in friends:
+    print('Hello',friend)
+print('done')
